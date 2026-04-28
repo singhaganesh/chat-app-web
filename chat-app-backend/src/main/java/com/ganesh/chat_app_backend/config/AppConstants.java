4df4ec54b8e4f8af974790d6fed740d71c71f6e4
Ganesh Singh <ganeshsingha741@gmail.com>
2026-04-28 14:34:24 +0530

    refactor(config): update frontend base URL in AppConstants

--- chat-app-backend/src/main/java/com/ganesh/chat_app_backend/config/AppConstants.java
+++ chat-app-backend/src/main/java/com/ganesh/chat_app_backend/config/AppConstants.java
@@ -1,5 +1,5 @@
 package com.ganesh.chat_app_backend.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL = "http://localhost:5173";
+    public static final String FRONT_END_BASE_URL = "https://chatapp.ganeshsingha.online/";
 }
