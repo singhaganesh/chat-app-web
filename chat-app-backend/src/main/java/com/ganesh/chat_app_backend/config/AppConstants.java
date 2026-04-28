96af49d0612d98686c6ddf791c6a495f1dff2131
Ganesh Singh <ganeshsingha741@gmail.com>
2026-04-28 15:02:40 +0530

    fix(config): remove trailing slash from frontend URL for CORS

--- chat-app-backend/src/main/java/com/ganesh/chat_app_backend/config/AppConstants.java
+++ chat-app-backend/src/main/java/com/ganesh/chat_app_backend/config/AppConstants.java
@@ -1,5 +1,5 @@
 package com.ganesh.chat_app_backend.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL = "https://chatapp.ganeshsingha.online/";
+    public static final String FRONT_END_BASE_URL = "https://chatapp.ganeshsingha.online";
 }
